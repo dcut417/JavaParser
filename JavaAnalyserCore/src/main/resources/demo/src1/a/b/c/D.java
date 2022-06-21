6ec8c5ee7032a34fd790bf2e67ca2d545c5e0d36
dcut417 <dcut417@aucklanduni.ac.nz>
2022-06-21 12:29:45 +1200

    Update D.java

--- JavaAnalyserCore/src/main/resources/demo/src1/a/b/c/D.java
+++ JavaAnalyserCore/src/main/resources/demo/src1/a/b/c/D.java
@@ -3,7 +3,7 @@ package a.b.c;
 /** This is a comment for the class */
 public class D {
     public void d(E anE) {
-    	int i = 0; //int declaration
+    	int i = 0; // This is a line comment
     	anE.eMethod("hello");
     }
 }
